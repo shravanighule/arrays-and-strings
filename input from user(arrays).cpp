1f9b7b3409d82572630a16c77d7687ba4763d789
shravanighule <shravanighule16@gmail.com>
2024-09-04 22:23:36 +0530

    Add files via upload

---
+++ input from user(arrays).cpp
@@ -0,0 +1,32 @@
+//shravanighule
+//23070123056
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int x[6];
+  cout<<"enter the element:"<<endl;
+  for (int j = 0; j<6; j++)
+  {
+
+    cin>>x[j];
+  }
+      cout<<"element:"<<endl;
+ 
+  for (int i = 0; i<6; i++)
+  {
+    cout<<x[i]<<endl;
+  }
+}
+/*output
+enter the element:
+10 20 30 40 50 60
+element:
+10
+20
+30
+40
+50
+60
+*/
