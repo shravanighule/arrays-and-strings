9f1e3a78b3186a53d1f955efa599c2d214666d5e
shravanighule <shravanighule16@gmail.com>
2024-09-04 22:14:42 +0530

    Add files via upload

---
+++ concatenation of strings.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string str1= "SHRAVANI";
+	string str2= "GHULE";
+	string result = str1+str2;
+	cout<<result<<endl;
+	return 0;
+	 
+}
+
+
+
+
+
+SHRAVANIGHULE
+
+--------------------------------
+Process exited after 0.1623 seconds with return value 0
+Press any key to continue . . .
+
+
