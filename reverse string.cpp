c32ab6a904ada17f3e1c721862fb55326b3ff0b1
shravanighule <shravanighule16@gmail.com>
2024-09-04 22:30:50 +0530

    Add files via upload

---
+++ reverse string.cpp
@@ -0,0 +1,19 @@
+//shravanighule
+//23070123056
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str = " SYMBI ";
+    int i;
+    cout<<"String in reverse is: "<<endl;
+    for(i=str.length()-1;i>0;i--)
+    {
+        cout<<str[i];
+    }
+}
+
+/*Output
+String in reverse is: 
+ IBMYS
+*/
