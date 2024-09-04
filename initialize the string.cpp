62d124aa4aa4b25503ad81e653191b84fb5511ce
shravanighule <shravanighule16@gmail.com>
2024-09-04 22:18:32 +0530

    Add files via upload

---
+++ initialize the string.cpp
@@ -0,0 +1,16 @@
+//shravanighule
+//23070123056
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str1[4]="C++";
+    cout<<str1<<endl;
+    char str2[]={'C','+','0'};
+    cout<<str2<<endl;
+}
+
+/* Output
+C++
+C + 0
+*/
