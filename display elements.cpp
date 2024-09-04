8433e31841395b4a2365e362899d39d172d0f4c7
shravanighule <shravanighule16@gmail.com>
2024-09-04 22:21:28 +0530

    Add files via upload

---
+++ display elements.cpp
@@ -0,0 +1,15 @@
+//shravanighule
+//23070123056
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x[6] = {10,20,30,40,50,60};
+    for (int i = 0; i<6; i++)
+    {
+        cout<<x[i]<<endl;
+    }
+    
+}
+
+
